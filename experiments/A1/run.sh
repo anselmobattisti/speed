4b64b788d97e571d0b25192ba084fb14d6333888
Anselmo Battisti <anselmo@webgenium.com.br>
2023-07-24 08:53:20 -0300

    Notebook for first graph.

--- experiments/A1/run.sh
+++ experiments/A1/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 BASEDIR=$(dirname $0)
+n=0
+maxjobs=2
 
 for file in "$BASEDIR"/files/*; do
 
@@ -16,15 +18,20 @@ for file in "$BASEDIR"/files/*; do
     export ALGORITHM="speed"
     python3 ../../main.py --logs "$log_path/speed" --config ./config/config_simulation.yml --zones ./config/zone_topology.yml --entities "$file" &
 
-    export BIGGEST_SEGMENT="True"
+#    export BIGGEST_SEGMENT="True"
     export ALGORITHM="random"
     python3 ../../main.py --logs "$log_path/random" --config ./config/config_simulation.yml --zones ./config/zone_topology.yml --entities "$file" &
 
-    export BIGGEST_SEGMENT="True"
+#    export BIGGEST_SEGMENT="True"
     export ALGORITHM="greedy"
     python3 ../../main.py --logs "$log_path/greedy" --config ./config/config_simulation.yml --zones ./config/zone_topology.yml --entities "$file" &
 
   fi
 
+  # limit jobs
+  if (( $(($((++n)) % $maxjobs)) == 0 )) ; then
+      wait # wait until all have finished (not optimal, but most times good enough)
+      echo $n wait
+  fi
 done;
 
